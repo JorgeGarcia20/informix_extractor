ed5a2aa0e4d7f71e7e61edf9483f25a153bd831a
JorgeGarcia20 <alberto-garcia20@outlook.com>
2024-01-29 22:25:57 -0600

    Nueva forma de extraer datos de ejemplo de las tablas

--- extract_data_samples.sql
+++ extract_data_samples.sql
@@ -7,14 +7,19 @@
 -- Importante colocar un limite ya que no se sabe la longitud filas existentes en cada tabla.
 
 
-SELECT 'Table1';
-SELECT * FROM table1 LIMIT 2;
+-- SELECT 'Table1';
+-- SELECT * FROM table1 LIMIT 2;
 
-SELECT 'Table2';
-SELECT * FROM table2 LIMIT 2;
+-- SELECT 'Table2';
+-- SELECT * FROM table2 LIMIT 2;
 
-SELECT 'Table3';
-SELECT * FROM table3 LIMIT 2;
+-- SELECT 'Table3';
+-- SELECT * FROM table3 LIMIT 2;
 
-SELECT 'Table4';
-SELECT * FROM table4 LIMIT 2;
+-- SELECT 'Table4';
+-- SELECT * FROM table4 LIMIT 2;
+
+SELECT t.*, "Table1" table_name FROM table1 AS t LIMIT 2;
+SELECT t.*, "Table2" table_name FROM table2 AS t LIMIT 2;
+SELECT t.*, "Table3" table_name FROM table3 AS t LIMIT 2;
+SELECT t.*, "Table4" table_name FROM table4 AS t LIMIT 2;
